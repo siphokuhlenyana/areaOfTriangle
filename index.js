06b8b586aa5158209a181dfa56d3c5bcf916e71e
Siphokuhle <siphokuhlenyana@gmail.com>
2024-06-04 15:21:16 +0200

    added prompt

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let s,m 
-function calc(s,m){
-return (1/2) *s*m
+let base=document.querySelectorAll('input')[0]
+let height=document.querySelectorAll('input')[1]
+let answer=document.querySelectorAll('input')[2]
+let btn=document.querySelector('button')
+
+
+// let s,m 
+// function calc(s,m){
+// return (1/2) *s*m
+// }
+// console.log(calc(10,2))
+
+function calc(){
+    return answer.value =(1/2)*base.value *height.value
 }
-console.log(calc(10,2))
+calc()
+btn.addEventListener('click',calc)
